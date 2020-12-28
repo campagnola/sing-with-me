54a5e8f869ff95f0b9e34a463cbfa265e958bdb3
Luke Campagnola <luke.campagnola@gmail.com>
2020-12-27 16:10:25 -0800

    refactor into classes, add type hinting

--- sing_with_me.py
+++ sing_with_me.py
@@ -1,126 +1,203 @@
 import sys, threading, atexit, queue, socket, time, argparse
+from typing import Optional, NamedTuple
 import numpy as np
 import pyaudio
 
-sample_rate = 22050
-frames_per_buffer = 256
-bytes_per_buffer = frames_per_buffer * 2
-buffer_duration = frames_per_buffer / sample_rate
 
-latency = 40e-3
-latency_buffers = int(latency / buffer_duration)
-
-empty_frame = b'\0' * bytes_per_buffer
-
-ring_buffer = np.zeros(frames_per_buffer * int(10.0 / buffer_duration), dtype='int16')
-read_pointer = 0
+class AudioChunk(NamedTuple):
+    data: np.ndarray
+    pointer: int
+    timestamp: float
 
 
-parser = argparse.ArgumentParser()
-#parser.add_argument('address', type=str, help='IP address to connect to')
-args = parser.parse_args()
+class RecordingStream:
+    def __init__(self, sample_rate, frames_per_buffer):
+        self.in_stream = pa.open(
+            format=pyaudio.paInt16,
+            channels=1,
+            rate=sample_rate,
+            input=True,
+            stream_callback=self.new_frame,
+            frames_per_buffer=frames_per_buffer,
+        )
 
+        self.connections = []
+        self.pointer = 0
 
+    def connect(self, target):
+        """Add new frame callback
+        """
+        self.connections.append(target)
 
-pa = pyaudio.PyAudio()
+    def stop(self):
+        self.in_stream.stop_stream()
+        self.in_stream.close()
 
-write_pointer = 0
+    def new_frame(self, data, n_frames, time_info, status):
+        arr = np.fromstring(data, dtype='int16')
 
-def in_callback(data, n_frames, time_info, status):
-    global write_pointer, read_pointer, ring_buffer
-
-    desired_write_pointer = int(read_pointer + latency * sample_rate)
-    if abs(write_pointer - desired_write_pointer) > (50e-3 * sample_rate):
-        print("adjust write pointer:", write_pointer, desired_write_pointer)
-        write_pointer = desired_write_pointer
-    ring_ptr = write_pointer % len(ring_buffer)
-    arr = np.fromstring(data, dtype=ring_buffer.dtype)
-
-    # write data into ring buffer, possibly in two parts if it wraps around to the beginning
-    first_size = min(len(arr), len(ring_buffer) - ring_ptr)
-    ring_buffer[ring_ptr:ring_ptr+first_size] += arr[:first_size]
-    if first_size < len(arr):
-        second_size = len(arr) - first_size
-        ring_buffer[:second_size] = arr[first_size:]
-    
-    write_pointer += len(arr)
-
-    return None, pyaudio.paContinue
-
-# def out_callback(data, n_frames, time_info, status):
-#     if in_queue.qsize() == 0:
-#         return empty_frame, pyaudio.paContinue
-#     while in_queue.qsize() > 2:
-#         in_queue.get()
-#     return in_queue.get()[1], pyaudio.paContinue
-
-in_stream = pa.open(
-    format=pyaudio.paInt16,
-    channels=1,
-    rate=sample_rate,
-    input=True,
-    stream_callback=in_callback,
-    frames_per_buffer=frames_per_buffer,
-)
-
-out_stream = pa.open(
-    format=pyaudio.paInt16,
-    channels=1,
-    rate=sample_rate,
-    output=True,
-)
-
-running = True
-
-# def record():
-#     last_time = time.time()
-#     while running:
-#         d = in_stream.read(8192)
-#         in_queue.put(d)
-#         now = time.time()
-#         print(len(d) / (now - last_time))
-#         last_time = now
-
-# in_thread = threading.Thread(target=record, daemon=True)
-# in_thread.start()
-
-
-def play():
-    global read_pointer, ring_buffer
-
-    start_time = time.time()
-
-    # pre-load so there is always at least 1 buffer of extra data queued
-    out_stream.write(empty_frame)
-
-    while running:
-        ring_ptr = read_pointer % len(ring_buffer)
-
-        # sleep until time to play the next chunk
-        now = time.time()
-        next_play_time = start_time + read_pointer / sample_rate
-        dt = next_play_time - now
-        if dt > 0:
-            time.sleep(dt)
+        d = AudioChunk(arr, self.pointer, time.time())
+        for target in self.connections:
+            target.add_buffer(d)
         
-        buf = ring_buffer[ring_ptr:ring_ptr+frames_per_buffer]
-        out_stream.write(buf.tostring())
-        ring_buffer[ring_ptr:ring_ptr+frames_per_buffer] = 0
-        read_pointer += frames_per_buffer
+        self.pointer += len(arr)
+        return None, pyaudio.paContinue
 
 
-out_thread = threading.Thread(target=play, daemon=True)
-out_thread.start()
+class PlayHead(NamedTuple):
+    pointer: int
+    timestamp: float
 
 
+class PlaybackStream:
+    ring_buffer: np.ndarray
+    play_head: PlayHead
+
+    def __init__(self, sample_rate:float, frames_per_buffer:int):
+        self.out_stream = pa.open(
+            format=pyaudio.paInt16,
+            channels=1,
+            rate=sample_rate,
+            frames_per_buffer=frames_per_buffer,
+            output=True,
+        )
+
+        self.sample_rate = sample_rate
+        self.frames_per_buffer = frames_per_buffer
+        self.buffer_duration = frames_per_buffer / sample_rate
+
+        self.ring_buffer = np.zeros(frames_per_buffer * int(10.0 / self.buffer_duration), dtype='int16')
+        self.play_head = PlayHead(0, 0)
+
+        self.running = True
+
+        self.thread = threading.Thread(target=self.run, daemon=True)
+        self.thread.start()
+
+    def run(self):
+        self.start_time = start_time = time.time()
+        ring_buffer = self.ring_buffer
+        frames_per_buffer = self.frames_per_buffer
+        sample_rate = self.sample_rate
+
+        # pre-load so there is always at least 1 buffer of extra data queued
+        self.out_stream.write(b'\0\0' * 512)
+
+        while self.running:
+            read_pointer = self.play_head.pointer
+            ring_ptr = read_pointer % len(ring_buffer)
+
+            # sleep until time to play the next chunk
+            now = time.time()
+            next_play_time = start_time + read_pointer / sample_rate
+            dt = (next_play_time - now) - 2e-3
+            if dt > 0:
+                time.sleep(dt)
+            
+            buf:np.ndarray = ring_buffer[ring_ptr:ring_ptr+frames_per_buffer]
+            self.out_stream.write(buf.tostring())
+            ring_buffer[ring_ptr:ring_ptr+frames_per_buffer] = 0
+            read_pointer += len(buf)
+            self.play_head = PlayHead(read_pointer, now)
+
+    def stop(self):
+        self.running = False
+        self.thread.join()
+        self.out_stream.stop_stream()
+        self.out_stream.close()
+
+    def index_at_time(self, time:float):
+        return int((time - self.start_time) * sample_rate)
+
+    def add_buffer_data(self, data:np.ndarray, index:int):
+        """add data into ring buffer, possibly in two parts if it wraps around to the beginning
+        """
+        ring_buffer = self.ring_buffer
+        ring_ptr = index % len(ring_buffer)
+
+        first_size = min(len(data), len(ring_buffer) - ring_ptr)
+        ring_buffer[ring_ptr:ring_ptr+first_size] += data[:first_size]
+        if first_size < len(data):
+            second_size = len(data) - first_size
+            ring_buffer[:second_size] = data[first_size:]
 
 
-def quit():
-    running = False
-    in_stream.stop_stream()
-    in_stream.close()
-    out_stream.stop_stream()
-    out_stream.close()
-    pa.terminate()
+class StreamWriter:
+    """Receive timestamped sound chunks, write into an output stream with a target latency
+    """
+    thread: Optional[threading.Thread]
+    data_queue: queue.PriorityQueue
 
-atexit.register(quit)
+    def __init__(self, in_stream:RecordingStream, out_stream:PlaybackStream, latency:float=100e-3, thread:bool=False):
+        self.in_stream = in_stream
+        self.out_stream = out_stream
+        self.latency = latency
+        self.data_queue = queue.PriorityQueue()
+        self.running = True
+
+        if thread:
+            self.thread = threading.Thread(target=self.run, daemon=True)
+            self.thread.start()
+        else:
+            self.thread = None
+
+        self.index_offset:int = 0
+
+        in_stream.connect(self)
+
+    def add_buffer(self, data:AudioChunk):
+        if not self.running:
+            return
+        if self.thread is None:
+            self.handle_data(data)
+        else:
+            self.data_queue.put(data)
+
+    def handle_data(self, data:AudioChunk):
+        sample_rate = self.out_stream.sample_rate
+        buf, index, play_time = data
+
+        last_play_index, last_play_time = self.out_stream.play_head
+        desired_play_time = play_time + self.latency
+        desired_play_index = last_play_index + int((desired_play_time - last_play_time) * sample_rate)
+        desired_index_offset = desired_play_index - last_play_index
+        if abs(self.index_offset - desired_index_offset) > (20e-3 * sample_rate):
+            print("adjust index offset:", self.index_offset, desired_index_offset)
+            self.index_offset = desired_index_offset
+
+        write_pointer = index + self.index_offset
+
+        self.out_stream.add_buffer_data(buf, write_pointer)
+
+    def stop(self):
+        self.running = False
+        if self.thread is not None:
+            self.thread.join()
+
+    def run(self):
+        while self.running:
+            data = self.data_queue.get()
+            self.handle_data(data)
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    #parser.add_argument('address', type=str, help='IP address to connect to')
+    args = parser.parse_args()
+
+
+    pa = pyaudio.PyAudio()
+
+    sample_rate = 22050
+
+    mic_stream = RecordingStream(sample_rate, frames_per_buffer=128)
+    out_stream = PlaybackStream(sample_rate, frames_per_buffer=128)
+    mic_writer = StreamWriter(mic_stream, out_stream, latency=20e-3)
+
+    def quit():
+        out_stream.stop()
+        mic_stream.stop()
+        pa.terminate()
+
+    atexit.register(quit)
